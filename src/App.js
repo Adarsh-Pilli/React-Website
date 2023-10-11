3261fa2a7f89c07a34f486d23ef05396b834f89d
Adarsh <adarshhero360@gmail.com>
2023-10-11 21:04:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My React Page</h1>
+        <p>Name: Pilli Adarsh</p>
+        <p>Enrollment Number: 20UCS055</p>
+        <p>Registration Number: 2012583</p>
+        <p>College: National Institute of Technology Agartala</p>
+        <p>Cloud Computing Assignment: Demo React App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
